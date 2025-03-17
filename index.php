f494c49333cd4c9ccc2656cc7191fb934922c324
Geojke <zhora.mosa@mail.ru>
2025-03-17 13:19:22 +0300

    changed file index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$title = 'Main Page';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,11 +9,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello Git</title>
+    <title><?= $title ?></title>
 </head>
 <body>
 <?php
-echo 'Hello Git!';
+    if (isset($title)) echo 'Hello Git!';
 ?>
 </body>
 </html>
